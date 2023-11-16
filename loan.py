871cb942be73826dc2422ad9bf4fcdd6e2ae3fbd
chandu <sekharycs18@gmail.com>
2023-11-16 18:18:35 +0530

    created a new file loan.py

---
+++ loan.py
@@ -0,0 +1,8 @@
+'''
+area = 1/2*base*height
+'''
+
+base=int(input("enter the value :"))
+height=int(input("enter the value :"))
+area=1/2*base*height
+print("the areaof a traingle with {} cm width and {} height is {} square cm".format(base,height,area))
